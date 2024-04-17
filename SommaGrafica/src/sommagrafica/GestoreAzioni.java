f7f834b22acc09a7db0c6f07215a5ed3fffe42f2
DandreaFrancesco <145185353+DANDREAFRA@users.noreply.github.com>
2024-04-17 13:48:59 +0200

    Add files via upload

---
+++ SommaGrafica/src/sommagrafica/GestoreAzioni.java
@@ -0,0 +1,18 @@
+
+package sommagrafica;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author UTENTE 1
+ */
+public class GestoreAzioni implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
